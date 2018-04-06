b996c2a1f87f1faccfc9039f324da83bb2e0ae59
RubenK01 <rbn994@gmail.com>
2018-04-06 23:15:17 +0200

    Initial commit

---
+++ src/Connection.java
@@ -0,0 +1,8 @@
+
+/**
+ * @uml.annotations 
+ *    uml_dependency="mmi:///#jsrctype^name=Statement[jcu^name=Statement.java[jpack^name=[jsrcroot^srcfolder=src[project^id=MySystem]]]]$uml.Interface"
+ */
+public interface Connection {
+
+}
---
+++ src/Statement.java
@@ -0,0 +1,8 @@
+
+/**
+ * @uml.annotations 
+ *    uml_dependency="mmi:///#jsrctype^name=ResultSet[jcu^name=ResultSet.java[jpack^name=[jsrcroot^srcfolder=src[project^id=MySystem]]]]$uml.Interface"
+ */
+public interface Statement {
+
+}
